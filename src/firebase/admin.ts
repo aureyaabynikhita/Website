35737e16e71065c10b1c2fe76da63221f4b01a16
Yashraj Jain <yashrajjain7869@gmail.com>
2026-07-20 14:58:24 +0530

    Update admin.ts

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -14,14 +14,6 @@ import { getAuth } from "firebase-admin/auth";
  */
 
 
-  return initializeApp({
-    credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    }),
-  });
-}
 
 const adminApp = getAdminApp();
 export const adminDb = getFirestore(adminApp);
